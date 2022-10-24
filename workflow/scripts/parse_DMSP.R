3fe59b231f5acd691d904de6893a4f3b44f79222
lucaz88 <luca.zoccarato@boku.ac.at>
2022-10-24 12:15:45 +0200

    fix parsing script

--- workflow/scripts/parse_DMSP.R
+++ workflow/scripts/parse_DMSP.R
@@ -1,27 +1,27 @@
-parse_DMSP = function(DMSP_files, 
+parse_DMSP <- function(DMSP_files, 
                       blast_DMSP_tab) {
   #! load packages
   suppressMessages(suppressWarnings(library(stringr)))
   suppressMessages(suppressWarnings(library(tidyverse)))
   
   #! import data
-  DMSP_ann1 = lapply(DMSP_files, function(i) 
+  DMSP_ann1 <- lapply(DMSP_files, function(i) 
     tryCatch(read.delim(i, h=F, quote="", fill=F, check.names = F), 
              error = function(x) cat("No DMSP related genes in", i,"\n")))
-  names(DMSP_ann1) = gsub(".tsv", "", basename(DMSP_files))
-  DMSP_ann1 = do.call(rbind, DMSP_ann1)
-  colnames(DMSP_ann1) = c("qseqid","sseqid","pident","length","mismatch","gapopen",
+  names(DMSP_ann1) <- gsub(".tsv", "", basename(DMSP_files))
+  DMSP_ann1 <- do.call(rbind, DMSP_ann1)
+  colnames(DMSP_ann1) <- c("qseqid","sseqid","pident","length","mismatch","gapopen",
                           "qstart","qend","sstart","send","evalue","bitscore","annotation") # standard BLAST output format 6
-  colnames(DMSP_ann1)[colnames(DMSP_ann1) == "qseqid"] = "locus_tag"
-  DMSP_ann1 = cbind(filename=gsub("\\.[0-9]+$", "", row.names(DMSP_ann1)), DMSP_ann1)
+  colnames(DMSP_ann1)[colnames(DMSP_ann1) == "qseqid"] <- "locus_tag"
+  DMSP_ann1 <- cbind(filename=gsub("\\.[0-9]+$", "", row.names(DMSP_ann1)), DMSP_ann1)
   
   #! filtering
-  DMSP_ann2 = DMSP_ann1
-  DMSP_ann2 = DMSP_ann2[DMSP_ann2$evalue < 1e-70, ] # filter based on min e-value - Gärdes et al., 2013
-  # DMSP_ann2 = DMSP_ann2[DMSP_ann2$V12 > 60, ] # filter based on min bit score
+  DMSP_ann2 <- DMSP_ann1
+  DMSP_ann2 <- DMSP_ann2[DMSP_ann2$evalue < 1e-70, ] # filter based on min e-value - Gärdes et al., 2013
+  # DMSP_ann2 <- DMSP_ann2[DMSP_ann2$V12 > 60, ] # filter based on min bit score
   
   #! get coherent gene names
-  DMSP_ann2$gene_ann = sapply(DMSP_ann2$annotation, function(i) {
+  DMSP_ann2$gene_ann <- sapply(DMSP_ann2$annotation, function(i) {
     ifelse(grepl("ddd", i, ignore.case = T), paste0("ddd", toupper(substr(gsub(".* ddd|.*=ddd", "", i, ignore.case = T), 1, 1))),
            ifelse(grepl("dmd", i, ignore.case = T), paste0("dmd", toupper(substr(gsub(".* dmd|.*=dmd", "", i, ignore.case = T), 1, 1))),
                   ifelse(grepl("acu", i, ignore.case = T), paste0("acu", toupper(substr(gsub(".* acu|.*=acu", "", i, ignore.case = T), 1, 1))), "NA"
@@ -29,39 +29,44 @@ parse_DMSP = function(DMSP_files,
   })
   
   #! parse blast output
-  DMSP_ann3 = do.call(rbind, lapply(split(DMSP_ann2, f = DMSP_ann2$locus_tag), function(i) {
+  DMSP_ann3 <- do.call(rbind, lapply(split(DMSP_ann2, f = DMSP_ann2$locus_tag), function(i) {
     #! find best hit
-    i2 = i[i$evalue == min(i$evalue) & i$bitscore == max(i$bitscore), , drop=F]
+    i2 <- i[i$evalue == min(i$evalue) & i$bitscore == max(i$bitscore), , drop=F]
     #! solve promiscuity (i.e. label as unclear if ties have different gene names)
     if (nrow(i2) > 1) {
-      i2 = i2[1, , drop=F]
+      i2 <- i2[1, , drop=F]
       if (length(unique(i$gene_ann)) == 1) {
-        i2[1, "gene_ann"] = unique(i$gene_ann)
-      } else { i2[1, "gene_ann"] = "unclear_ann" }
+        i2[1, "gene_ann"] <- unique(i$gene_ann)
+      } else { i2[1, "gene_ann"] <- "unclear_ann" }
     }
     return(i2)
   }))
   
+  #! handle exception of no hits passing the filtering step 
+  if (is.null(DMSP_ann3)) {
+    DMSP_ann3 <- DMSP_ann2
+  }
+  
   #! check for completeness
-  DMSP_ann4 = DMSP_ann3 %>% 
+  DMSP_ann4 <- DMSP_ann3 %>% 
     mutate(value = 1) %>% 
     pivot_wider(id_cols = filename, names_from = gene_ann, names_sort = T,
                 values_from = value, values_fn = sum, values_fill = 0)
-  min_genes_1 = 1 # out of 1 as ddd|ALMA genes are all redundant 
-  min_genes_2 = 3 # out of 4 for the dmd patway 
+  min_genes_1 <- 1 # out of 1 as ddd|ALMA genes are all redundant 
+  min_genes_2 <- 3 # out of 4 for the dmd patway 
   # See 10.1146/annurev-marine-120710-100827 and 10.3389/fmicb.2017.00637
-  DMSP_ann5 = data.frame(filename=DMSP_ann4$filename,
-                         DMSP_cleavage=apply(DMSP_ann4[, grepl("ddd|ALMA", colnames(DMSP_ann4))], 1, 
+  DMSP_ann5 <- data.frame(filename = DMSP_ann4$filename,
+                         DMSP_cleavage = apply(DMSP_ann4[, grepl("ddd|ALMA", colnames(DMSP_ann4))], 1, 
                                              function(x) ifelse(sum(x>0) >= min_genes_1, 1, 0)),
-                         DMSP_demethylation=apply(DMSP_ann4[, grepl("dmd", colnames(DMSP_ann4))], 1, 
+                         DMSP_demethylation = apply(DMSP_ann4[, grepl("dmd", colnames(DMSP_ann4))], 1, 
                                                   function(x) ifelse(sum(x>0) >= min_genes_2, 1, 0)))
   
   #! select genes
-  DMSP_ann6 = DMSP_ann3[(DMSP_ann3$filename %in% DMSP_ann5$filename[DMSP_ann5$DMSP_cleavage > 0] &
+  DMSP_ann6 <- DMSP_ann3[(DMSP_ann3$filename %in% DMSP_ann5$filename[DMSP_ann5$DMSP_cleavage > 0] &
                            grepl("ddd|ALMA", DMSP_ann3$gene_ann)) |
                           (DMSP_ann3$filename %in% DMSP_ann5$filename[DMSP_ann5$DMSP_demethylation > 0] 
                            & grepl("dmd", DMSP_ann3$gene_ann)), ]
-  DMSP_ann6$trait = ifelse(grepl("ddd|ALMA", DMSP_ann6$gene_ann), "DMSP_cleavage",
+  DMSP_ann6$trait <- ifelse(grepl("ddd|ALMA", DMSP_ann6$gene_ann), "DMSP_cleavage",
                            ifelse(grepl("dmd", DMSP_ann6$gene_ann), "DMSP_demethylation", NA))
   
   #! save output
--- workflow/scripts/parse_vibrioferrin.R
+++ workflow/scripts/parse_vibrioferrin.R
@@ -1,62 +1,67 @@
-parse_phytohormones = function(vibferr_files, 
+parse_phytohormones <- function(vibferr_files, 
                                blast_vibrioferrin_tab) {
   #! load packages
   suppressMessages(suppressWarnings(library(stringr)))
   suppressMessages(suppressWarnings(library(tidyverse)))
   
   #! import data
-  vibferr_ann1 = lapply(vibferr_files, function(i) 
+  vibferr_ann1 <- lapply(vibferr_files, function(i) 
     tryCatch(read.delim(i, h=F, quote="", fill=F, check.names = F), 
              error = function(x) cat("No vibrioferrin related genes in", i,"\n")))
-  names(vibferr_ann1) = gsub(".tsv", "", basename(vibferr_files))
-  vibferr_ann1 = do.call(rbind, vibferr_ann1)
-  colnames(vibferr_ann1) = c("qseqid","sseqid","pident","length","mismatch","gapopen",
+  names(vibferr_ann1) <- gsub(".tsv", "", basename(vibferr_files))
+  vibferr_ann1 <- do.call(rbind, vibferr_ann1)
+  colnames(vibferr_ann1) <- c("qseqid","sseqid","pident","length","mismatch","gapopen",
                              "qstart","qend","sstart","send","evalue","bitscore","annotation") # standard BLAST output format 6
-  colnames(vibferr_ann1)[colnames(vibferr_ann1) == "qseqid"] = "locus_tag"
-  vibferr_ann1 = cbind(filename=gsub("\\.[0-9]+$", "", row.names(vibferr_ann1)), vibferr_ann1)
+  colnames(vibferr_ann1)[colnames(vibferr_ann1) == "qseqid"] <- "locus_tag"
+  vibferr_ann1 <- cbind(filename=gsub("\\.[0-9]+$", "", row.names(vibferr_ann1)), vibferr_ann1)
   
   #! filtering
-  vibferr_ann2 = vibferr_ann1
-  vibferr_ann2 = vibferr_ann2[vibferr_ann2$evalue < 1e-5, ] # filter based on min e-value, Amin et al, 2012
-  vibferr_ann2 = vibferr_ann2[vibferr_ann2$pident > 30, ] # filter based on min similarity, Amin et al, 2012
+  vibferr_ann2 <- vibferr_ann1
+  vibferr_ann2 <- vibferr_ann2[vibferr_ann2$evalue < 1e-5, ] # filter based on min e-value, Amin et al, 2012
+  vibferr_ann2 <- vibferr_ann2[vibferr_ann2$pident > 30, ] # filter based on min similarity, Amin et al, 2012
   
   #! get coherent gene names
-  vibferr_ann2$gene_ann = sapply(vibferr_ann2$annotation, function(i) {
+  vibferr_ann2$gene_ann <- sapply(vibferr_ann2$annotation, function(i) {
     ifelse(grepl("pvu", i, ignore.case = T), paste0("pvu", substr(gsub(".*pvu", "", i, ignore.case = T), 1, 1)),
            ifelse(grepl("pvs", i, ignore.case = T), paste0("pvs", substr(gsub(".*pvs", "", i, ignore.case = T), 1, 1)), NA
            ))
   }) 
   
   #! parse blast output
-  vibferr_ann3 = do.call(rbind, lapply(split(vibferr_ann2, f = vibferr_ann2$locus_tag), function(i) {
+  vibferr_ann3 <- do.call(rbind, lapply(split(vibferr_ann2, f = vibferr_ann2$locus_tag), function(i) {
     #! find best hit
-    i2 = i[i$evalue == min(i$evalue) & i$bitscore == max(i$bitscore), , drop=F]
+    i2 <- i[i$evalue == min(i$evalue) & i$bitscore == max(i$bitscore), , drop=F]
     #! solve promiscuity (i.e. label as unclear if ties have different gene names)
     if (nrow(i2) > 1) {
-      i2 = i2[1, , drop=F]
+      i2 <- i2[1, , drop=F]
       if (length(unique(i$gene_ann)) == 1) {
-        i2[1, "gene_ann"] = unique(i$gene_ann)
-      } else { i2[1, "gene_ann"] = "unclear_ann" }
+        i2[1, "gene_ann"] <- unique(i$gene_ann)
+      } else { i2[1, "gene_ann"] <- "unclear_ann" }
     }
     return(i2)
   }))
   
+  #! handle exception of no hits passing the filtering step 
+  if (is.null(vibferr_ann3)) {
+    vibferr_ann3 <- vibferr_ann2
+  }
+  
   #! check for completeness
-  vibferr_ann4 = vibferr_ann3 %>% 
+  vibferr_ann4 <- vibferr_ann3 %>% 
     mutate(value = 1) %>% 
     pivot_wider(id_cols = filename, names_from = gene_ann, names_sort = T,
                 values_from = value, values_fn = sum, values_fill = 0)
-  min_genes = 4 # out of 5 total genes in both operons
-  vibferr_ann5 = data.frame(filename=vibferr_ann4$filename,
+  min_genes <- 4 # out of 5 total genes in both operons
+  vibferr_ann5 <- data.frame(filename=vibferr_ann4$filename,
                             pvsABCDE=apply(vibferr_ann4[, grepl("pvs", colnames(vibferr_ann4))], 1, function(x) ifelse(sum(x>0) >= min_genes, 1, 0)),
                             pvuABCDE=apply(vibferr_ann4[, grepl("pvu", colnames(vibferr_ann4))], 1, function(x) ifelse(sum(x>0) >= min_genes, 1, 0)))
   
   #! select genes
-  vibferr_ann6 = vibferr_ann3[(vibferr_ann3$filename %in% vibferr_ann5$filename[vibferr_ann5$pvsABCDE > 0] &
+  vibferr_ann6 <- vibferr_ann3[(vibferr_ann3$filename %in% vibferr_ann5$filename[vibferr_ann5$pvsABCDE > 0] &
                                  grepl("pvs", vibferr_ann3$gene_ann)) |
                                 (vibferr_ann3$filename %in% vibferr_ann5$filename[vibferr_ann5$pvuABCDE > 0] 
                                  & grepl("pvu", vibferr_ann3$gene_ann)), ]
-  vibferr_ann6$trait = ifelse(grepl("pvs", vibferr_ann6$gene_ann), "pvsABCDE",
+  vibferr_ann6$trait <- ifelse(grepl("pvs", vibferr_ann6$gene_ann), "pvsABCDE",
                               ifelse(grepl("pvu", vibferr_ann6$gene_ann), "pvuABCDE", NA))
   
   #! save output
